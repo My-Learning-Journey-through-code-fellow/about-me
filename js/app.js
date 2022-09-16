8ee37e8e9295284adf97671e1aa1ea3ed79da484
Trace Dugar <tracedugar89@gmail.com>
2022-09-15 20:07:35 -0700

    bug fixes

--- js/app.js
+++ js/app.js
@@ -12,9 +12,7 @@ alert(`Welcome to my site ${visitorName}! Please Answer these 5 yes or no questi
 
 function firstQuestion() {
   let questionOneGuess = prompt('Do you like music?').toLowerCase();
-
   console.log('Q1 >>>', questionOneGuess)
-
   if (questionOneGuess === 'y' || questionOneGuess === 'yes') {
     alert('Me too');
     score++;
@@ -22,75 +20,11 @@ function firstQuestion() {
     alert('How?')
   }
 }
-
 firstQuestion();
 
-let questionTwoGuess = prompt('Do you like sweets?').toLowerCase();
-
-console.log('Q2 >>>', questionTwoGuess)
-
-if (questionTwoGuess === 'y' || questionTwoGuess === 'yes') {
-  alert('Sweeeeeeeeeeeeet!');
-  score++;
-} else if (questionTwoGuess === 'n' || questionTwoGuess === 'no') {
-  alert('Are you okay?')
-}
-
-let questionThreeGuess = prompt('Do you like being outdoors?').toLowerCase();
-
-console.log('Q3 >>>', questionThreeGuess)
-
-if (questionThreeGuess === 'y' || questionThreeGuess === 'yes') {
-  alert('Sunshine is the best, isnt it?');
-  score++;
-} else if (questionThreeGuess === 'n' || questionThreeGuess === 'no') {
-  alert('GO GET SOME SUN!')
-}
-
-let questionFourGuess = prompt('Do you like animals?').toLowerCase();
-
-console.log('Q4 >>>', questionFourGuess)
-
-if (questionFourGuess === 'y' || questionFourGuess === 'yes') {
-  alert('You are a kind soul.');
-  score++;
-} else if (questionFourGuess === 'n' || questionFourGuess === 'no') {
-  alert('HOW COULD YOU NOT!')
-}
-
-let questionFiveGuess = prompt('Is your day good?').toLowerCase();
-
-console.log('Q5 >>>', questionFiveGuess)
-
-if (questionFiveGuess === 'y' || questionFiveGuess === 'yes') {
-  alert('Hell Yea!!!');
-  score++;
-} else if (questionFiveGuess === 'n' || questionFiveGuess === 'no') {
-  alert('There is always tomorrow')
-}
-
-// in collaboration with Hunter Fehr, and Chris Johnston 
-let myNum = 777;
-
-let guessNum = 4;
-
-while (guessNum > 0) {
-  let userNumGuess = prompt('What is my favorite number?');
-  guessNum--;
-  console.log('NumberGuess >>>', userNumGuess)
-  if (parseInt(userNumGuess) === myNum) {
-    alert("Correct, Great job!!")
-    score++;
-  } else if (questionOneGuess === 'n' || questionOneGuess === 'no') {
-    alert('How?')
-  }
-}
-firstQuestion();
 function secondQuestion() {
   let questionTwoGuess = prompt('Do you like sweets?').toLowerCase();
-
   console.log('Q2 >>>', questionTwoGuess)
-
   if (questionTwoGuess === 'y' || questionTwoGuess === 'yes') {
     alert('Sweeeeeeeeeeeeet!');
     score++;
@@ -99,11 +33,10 @@ function secondQuestion() {
   }
 }
 secondQuestion();
+
 function thirdQuestion() {
   let questionThreeGuess = prompt('Do you like being outdoors?').toLowerCase();
-
   console.log('Q3 >>>', questionThreeGuess)
-
   if (questionThreeGuess === 'y' || questionThreeGuess === 'yes') {
     alert('Sunshine is the best, isnt it?');
     score++;
@@ -112,11 +45,10 @@ function thirdQuestion() {
   }
 }
 thirdQuestion();
+
 function fourthQuestion() {
   let questionFourGuess = prompt('Do you like animals?').toLowerCase();
-
   console.log('Q4 >>>', questionFourGuess)
-
   if (questionFourGuess === 'y' || questionFourGuess === 'yes') {
     alert('You are a kind soul.');
     score++;
@@ -125,11 +57,10 @@ function fourthQuestion() {
   }
 }
 fourthQuestion();
+
 function fifthQuestion() {
   let questionFiveGuess = prompt('Is your day good?').toLowerCase();
-
   console.log('Q5 >>>', questionFiveGuess)
-
   if (questionFiveGuess === 'y' || questionFiveGuess === 'yes') {
     alert('Hell Yea!!!');
     score++;
@@ -138,6 +69,7 @@ function fifthQuestion() {
   }
 }
 fifthQuestion();
+
 // in collaboration with Hunter Fehr, and Chris Johnston 
 function numGuessGame() {
   let myNum = 777;
@@ -182,8 +114,8 @@ function guessGame() {
   if (guessNumQ7 === 0) {
     alert("New Orleans, Orleans Parish, Louisiana, the South, United States. Is the birthplace of Jazz.")
   }
-  {
-    alert(`Thank you, ${visitorName} for your participation! You scored ${score} of 7`)
-  }
 }
 guessGame();
+{
+  alert(`Thank you, ${visitorName} for your participation! You scored ${score} of 7`)
+}
